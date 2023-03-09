5055ae3d36f9587188987ffbb2fbd82ca842ab8b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2023-03-09 09:33:28 -0500

    Create a layout page to include header on nested routes

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home({ data }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Header />
+      {/* <Header /> */}
       <Search />
       {/* dropdown: filter by region */}
 
