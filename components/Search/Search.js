9cc8551c05d36d9a820d5e233adb0e7c0c0b98a0
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2023-03-11 18:10:35 -0500

    Submit functionality finished

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,22 +1,39 @@
+import { useMemo, useState } from "react";
+
 import { QUERIES } from "../../constants";
 import { Search as SearchIcon } from "react-feather";
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 function Search() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const onSubmit = useMemo(() => {
+    function searchCountry(e) {
+      e.preventDefault();
+      router.push(`/${search}`);
+    }
+
+    return searchCountry;
+  }, [search]);
+
   return (
-    <Wrapper>
+    <Wrapper onSubmit={onSubmit}>
       <VisuallyHidden htmlFor="search">Search for a country:</VisuallyHidden>
       <SearchIcon size={16} color="var(--gray-400)" />
       <TextInput
         type="text"
         placeholder="Search for a country..."
         id="search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.form`
   box-shadow: 0px 2px 4px hsla(0, 0%, 0%, 0.05);
   border-radius: ${5 / 16}rem;
   background-color: var(--white);
