d024eecae24df9ce5da6e17dd2a64b0f7bf73457
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2023-03-12 15:11:24 -0400

    WIP: Select

--- components/Select/Select.js
+++ components/Select/Select.js
@@ -1,4 +1,10 @@
+import { useEffect, useState } from "react";
+
+import useSWR from "swr";
+
 const Select = () => {
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const [selectedRegion, setSelectedRegion] = useState("");
   const regions = [
     { key: "africa", name: "Africa" },
     { key: "america", name: "America" },
@@ -7,8 +13,19 @@ const Select = () => {
     { key: "oceania", name: "Oceania" },
   ];
 
+  // async function getCountriesInRegion() {
+  const ENDPOINT = `https://restcountries.com/v3.1/region/${selectedRegion}`;
+
+  const { data, error, isLoading } = useSWR(ENDPOINT, fetcher);
+  console.log({ data, error, isLoading });
+  // }
+
+  // useEffect(() => {
+  //   getCountriesInRegion();
+  // }, [selectedRegion]);
+
   return (
-    <select>
+    <select onChange={(e) => setSelectedRegion(e.target.value)}>
       <option value="all">Filter by Region</option>
       {regions.map((region) => (
         <option key={region.key} value={region.name}>
