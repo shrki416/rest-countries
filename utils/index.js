954da3b60663738f573d349ae598e440a72fdb21
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2023-03-19 19:52:40 -0400

    WIP: working w/images 🤦‍

--- utils/index.js
+++ utils/index.js
@@ -5,3 +5,16 @@ export const formatNumber = (number) => {
 export const toLowerWithHyphen = (string) => {
   return string.toLowerCase().replace(/ /g, "-");
 };
+
+export async function getImageDimensions(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => {
+      resolve({ width: img.width, height: img.height });
+    };
+    img.onerror = () => {
+      reject(`Failed to load image: ${url}`);
+    };
+    img.src = url;
+  });
+}
